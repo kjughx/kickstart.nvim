b33e95efeea590d123cefdb6781d85b4d69673e5
Philip Johansson <philjn@axis.com>
2024-04-25 14:59:09 +0200

    Refactor
    Change-Id: I000c0120e1be6d2093f91ad2752d2ceda7287c34

--- init.lua
+++ init.lua
@@ -1,24 +1,8 @@
-local vim = vim
-
 -- This needs to be first
 require 'opts'
+
 require 'autocmds'
-
--- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-end ---@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
-
--- NOTE: Here is where you install your plugins.
-require("lazy").setup("plugins", {
-  change_detection = {
-    notify = false,
-  },
-})
+require 'lazyvim'
 
 -- Do this after loading plugins to override them
 --  Those that are loaded lazily needs more care
