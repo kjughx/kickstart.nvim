5a4eb8dc89235c1292cb733062fc79d90641614c
Philip J. <kjughx@gmail.com>
2024-12-16 08:12:27 +0100

    Add diffview dependency for neogit

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,6 +5,7 @@ return {
     'kjughx/neogit',
     dependencies = {
       'nvim-lua/plenary.nvim',
+      'sindrets/diffview.nvim',
     },
     config = function()
       require('neogit').setup {
